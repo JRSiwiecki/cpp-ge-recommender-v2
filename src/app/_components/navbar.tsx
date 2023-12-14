a43a6922161d5a75d4abe80dd2ebf3e608235231
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-13 18:16:39 -0800

    Add navbar component with temporary links

---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function navbar() {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <Link href="/">GE Recommender Tool</Link>
+      <Link href="/">Top Courses</Link>
+    </div>
+  );
+}
