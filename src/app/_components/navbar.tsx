9eea35da18395bcbdd7a83ba0a5e42d2de939f86
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-13 19:51:40 -0800

    Update padding, add horizontal align to links, update font sizes

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
-const linkStylingClasses = `rounded-md border p-2 text-center text-xl 
-        text-yellow-400 transition duration-300 hover:bg-green-800`;
+const linkStylingClasses = `rounded-md border p-2 text-center text-l 
+        text-yellow-400 transition duration-300 hover:bg-green-800 flex items-center m-1`;
 
 export default function navbar() {
   return (
-    <div className="flex flex-grow flex-row justify-evenly bg-neutral-950 p-3">
+    <div className="flex flex-grow flex-row justify-evenly bg-neutral-950 p-2">
       <Link href="/" className={linkStylingClasses}>
         Home
       </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-neutral-900 p-3">
-      <h1 className="text-center text-6xl text-white">CPP GE Recommender</h1>
+      <h1 className="text-center text-4xl text-white">CPP GE Recommender</h1>
     </main>
   );
 }
