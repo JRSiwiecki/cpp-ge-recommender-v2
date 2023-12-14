ff903161afab8b10a8d8cc33da9928c98774879e
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-13 19:40:25 -0800

    Extract common classes for links to variable

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,37 +1,24 @@
 import Link from "next/link";
 
+const linkStylingClasses = `rounded-md border p-2 text-center text-xl 
+        text-yellow-400 transition duration-300 hover:bg-green-800`;
+
 export default function navbar() {
   return (
     <div className="flex flex-grow flex-row justify-evenly bg-neutral-950 p-3">
-      <Link
-        href="/"
-        className="rounded-md border p-2 text-center text-xl 
-        text-yellow-400 transition duration-300 hover:bg-green-800"
-      >
+      <Link href="/" className={linkStylingClasses}>
         Home
       </Link>
 
-      <Link
-        href="/"
-        className="rounded-md border p-2 text-center text-xl 
-        text-yellow-400 transition duration-300 hover:bg-green-800"
-      >
+      <Link href="/" className={linkStylingClasses}>
         GE Recommender
       </Link>
 
-      <Link
-        href="/"
-        className="rounded-md border p-2 text-center text-xl 
-        text-yellow-400 transition duration-300 hover:bg-green-800"
-      >
+      <Link href="/" className={linkStylingClasses}>
         Top Courses
       </Link>
 
-      <Link
-        href="/"
-        className="rounded-md border p-2 text-center text-xl 
-        text-yellow-400 transition duration-300 hover:bg-green-800"
-      >
+      <Link href="/" className={linkStylingClasses}>
         About
       </Link>
     </div>
