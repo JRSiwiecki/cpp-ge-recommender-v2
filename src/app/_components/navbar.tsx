79399dfed67f6660fc6e70721783a5dd0875ad45
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-13 18:41:05 -0800

    Update colors, finish navbar styling

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -2,10 +2,30 @@ import Link from "next/link";
 
 export default function navbar() {
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <Link href="/">GE Recommender Tool</Link>
-      <Link href="/">Top Courses</Link>
+    <div className="flex flex-grow flex-row justify-evenly bg-neutral-950 p-3">
+      <Link
+        href="/"
+        className="rounded-md border p-2 text-center text-xl 
+        text-yellow-400 transition duration-300 hover:bg-green-800"
+      >
+        Home
+      </Link>
+
+      <Link
+        href="/"
+        className="rounded-md border p-2 text-center text-xl 
+        text-yellow-400 transition duration-300 hover:bg-green-800"
+      >
+        GE Recommender
+      </Link>
+
+      <Link
+        href="/"
+        className="rounded-md border p-2 text-center text-xl 
+        text-yellow-400 transition duration-300 hover:bg-green-800"
+      >
+        Top Courses
+      </Link>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-gray-950 p-3">
-      <h1 className="text-center text-6xl text-gray-300">CPP GE Recommender</h1>
+    <main className="flex min-h-screen flex-col items-center bg-neutral-900 p-3">
+      <h1 className="text-center text-6xl text-white">CPP GE Recommender</h1>
     </main>
   );
 }
