907c048e02c3cb8f48534db38d101229b53b3ecd
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-14 18:29:49 -0800

    Add docstrings for most functions, refactor for exceptions

--- src/scripts/scraper.py
+++ src/scripts/scraper.py
@@ -2,13 +2,54 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-# Only valid catalog urls, catalogs before 2021 have some varying structure that
-# I will not be dealing with!
 CATALOG_URLS = {
     2021: "https://catalog.cpp.edu/preview_program.php?catoid=57&poid=14912",
     2022: "https://catalog.cpp.edu/preview_program.php?catoid=61&poid=15936",
     2023: "https://catalog.cpp.edu/preview_program.php?catoid=65&poid=17161",
 }
+"""
+Only valid catalog urls, catalogs before 2021 have some varying structure that
+I will not be dealing with!
+"""
+
+VALID_YEARS = [2021, 2022, 2023]
+"""
+Valid years for CPP catalogs.
+"""
+
+LANGUAGE_CLASSES_FILTER = [
+    "Chinese",
+    "French",
+    "Spanish",
+    "German",
+]
+"""
+List of most language classes offered at CPP so that we can avoid adding them
+to results.
+"""
+
+VALID_AREA_SECTIONS = [
+    "A1",
+    "A2",
+    "A3",
+    "B1",
+    "B2",
+    "B4",
+    "B5",
+    "C1",
+    "C2",
+    "C3",
+    "D1",
+    "D2",
+    "D4",
+    "E0",
+    "F0",
+    "E",
+    "F",
+]
+"""
+Valid area sections that exist for CPP classes.
+"""
 
 
 def scrape_cpp_data(catalog_year: int):
@@ -23,13 +64,11 @@ def scrape_cpp_data(catalog_year: int):
         ResultSet[Any]: ResultSet containing the scraped data separated by class area.
     """
 
-    if type(catalog_year) is not int:
+    if not isinstance(catalog_year, int):
         raise TypeError("Invalid input, catalog_year must be an int.")
 
-    accepted_years = [2021, 2022, 2023]
-
-    if catalog_year not in accepted_years:
-        raise ValueError("Invalid catalog year.")
+    if catalog_year not in VALID_YEARS:
+        raise ValueError(f"{catalog_year} is not a valid catalog year.")
 
     url = CATALOG_URLS.get(catalog_year)
     page = requests.get(url)
@@ -39,7 +78,18 @@ def scrape_cpp_data(catalog_year: int):
     return class_areas
 
 
-def categorize_courses(class_areas):
+def categorize_courses(scraped_class_areas) -> list[dict]:
+    """
+    Categorizes scraped data into area_map and section_map which contains
+    an area's sections and a section's courses respectively.
+
+    Args:
+        scraped_class_areas (ResultSet[Any]): Scraped data from the CPP catalog that contains
+        all areas for a catalog year.
+
+    Returns:
+        list[dict]: A list containing first the area_map and second the section_map.
+    """
     # top element is current area
     area_stack = []
 
@@ -52,7 +102,7 @@ def categorize_courses(class_areas):
     # contains list of classes in that section
     section_map = {}
 
-    for class_area in class_areas:
+    for class_area in scraped_class_areas:
         area = class_area.find("h2")
         section = class_area.find("h3")
 
@@ -95,70 +145,63 @@ def categorize_courses(class_areas):
             for span in spans:
                 if span:
                     end_marker = span.text.index("(")
-
                     section_map[current_section].append(span.text[0 : end_marker - 1])
 
     # hard code solution for E and F
     area_map["E"].append("0. Lifelong Learning and Self-Development")
     area_map["F"].append("0. Ethnic Studies")
 
+    return [area_map, section_map]
 
-def get_opencpp_api_data():
+
+def get_opencpp_api_data() -> dict:
+    """
+    Grabs all course data from the OpenCPP API. This API's source code can be found here:
+    https://github.com/ZombiMigz/opencpp-api
+
+    Returns:
+        dict: Returns a dictionary/JSON object from the OpenCPP API data.
+    """
     response = requests.post("https://cpp-scheduler.herokuapp.com/data/courses/find")
     json_object = json.loads(response.text)
     return json_object
 
 
-language_classes_filter = [
-    "Chinese",
-    "French",
-    "Spanish",
-    "German",
-]
+def recommend_courses(area_section, area_map, section_map, json_object):
+    """
+    Recommends courses based on the area_section requested, sorts them by
+    highest average GPA to lowest average GPA by students who have taken the course.
 
+    Args:
+        area_section (list[str]): List containing the area at index 0 and section at index 1.
+        area_map (dict): Dict that maps an area to its corresponding sections.
+        section_map (dict): Dict that maps a section to its corresponding courses.
+        json_object (dict): Dict that contains the scraped data for its corresponding year.
 
-def recommend_course(area_section, area_map, section_map, json_object):
+    Returns:
+        list: List containing courses for the given area_section sorted by highest average GPA.
+    """
     requested_data = area_section
 
-    if len(requested_data) < 2:
-        response = {"Message": "Input too short."}
-        return json.dumps(response)
+    if len(requested_data) != 2:
+        raise ValueError(
+            f"Requested area section: {requested_data} must be 2 characters."
+        )
 
     requested_area = requested_data[0].upper()
     requested_section = requested_data[1]
 
-    accepted_area_sections = [
-        "A1",
-        "A2",
-        "A3",
-        "B1",
-        "B2",
-        "B4",
-        "B5",
-        "C1",
-        "C2",
-        "C3",
-        "D1",
-        "D2",
-        "D4",
-        "E",
-        "F",
-    ]
-
-    if requested_area not in accepted_area_sections:
-        response = {"Message": "Invalid area section."}
+    if requested_area not in VALID_AREA_SECTIONS:
+        raise ValueError(f"{requested_area} is an invalid area section.")
 
     if requested_area.isdigit():
-        response = {"Message": "Area must be a letter."}
-        return json.dumps(response)
+        raise ValueError(f"Area: {requested_area} must be a character.")
 
     if requested_section.isalpha():
-        response = {"Message": "Section must be a number."}
-        return json.dumps(response)
+        raise ValueError(f"Section: {requested_section} must be a number.")
 
     if requested_area not in area_map:
-        response = {"Message": "Area does not exist."}
-        return json.dumps(response)
+        raise ValueError(f"Area: {requested_area} does not exist.")
 
     found_sections = area_map[requested_area]
 
@@ -170,14 +213,12 @@ def recommend_course(area_section, area_map, section_map, json_object):
             break
 
     if not found_classes:
-        response = {"Message": "No sections found."}
-        return json.dumps(response)
+        raise ValueError("No sections found for this query.")
 
     course_codes = []
 
     for found_class in found_classes:
         end_marker = found_class.index("-") - 1
-
         course_codes.append(found_class[0:end_marker])
 
     course_gpas = []
@@ -201,7 +242,7 @@ def recommend_course(area_section, area_map, section_map, json_object):
                 # Remove language courses but only when looking in C2 courses
                 if area_section == "C2":
                     if course_title is not None and (
-                        any(lang in course_title for lang in language_classes_filter)
+                        any(lang in course_title for lang in LANGUAGE_CLASSES_FILTER)
                     ):
                         continue
 
@@ -229,13 +270,25 @@ def recommend_course(area_section, area_map, section_map, json_object):
                     ]
                 )
 
+    # Sort by highest average GPA
     course_gpas = sorted(course_gpas, key=lambda x: x[2], reverse=True)
-
-    result_json = json.dumps(course_gpas)
-    return result_json
+    return course_gpas
 
 
-def get_top_courses(area_map, section_map, json_object):
+def get_top_courses(area_map, section_map, json_object) -> dict:
+    """
+    Returns the top five courses for each area section.
+
+    TODO: Perhaps add another argument to return the top X courses for each area?
+
+    Args:
+        area_map (dict): Dict that maps an area to its corresponding sections.
+        section_map (dict): Dict that maps a section to its corresponding courses.
+        json_object (dict): Dict that contains OpenCPP API data about all courses.
+
+    Returns:
+        dict: Returns a dict containing the top courses for each area section.
+    """
     top_courses = {}
 
     for area in area_map.keys():
@@ -278,7 +331,7 @@ def get_top_courses(area_map, section_map, json_object):
                             if course_title is not None and (
                                 any(
                                     lang in course_title
-                                    for lang in language_classes_filter
+                                    for lang in LANGUAGE_CLASSES_FILTER
                                 )
                             ):
                                 continue
@@ -314,13 +367,24 @@ def get_top_courses(area_map, section_map, json_object):
             area_section_title = f"{area}{section}"
             top_courses[area_section_title] = section_courses[:5]
 
-    result_json = json.dumps(top_courses)
-
-    return result_json
+    return top_courses
 
 
-# Used in case the data from OpenCPP API doesn't contain a course title
-def get_course_title(full_course_name):
+def get_course_title(full_course_name: str) -> str:
+    """
+    Used in case the data from OpenCPP API doesn't contain a course title,
+    grabs course name from scraped CPP catalog data.
+
+    Args:
+        full_course_name (str): The full course name, containing typically course code and name.
+
+    Returns:
+        str: The course name with the course code removed.
+    """
+
+    if not isinstance(full_course_name, str):
+        raise TypeError("Invalid full_course_name type, must be a string.")
+
     # start 2 characters after the " - " separating class code from title
     start_marker = full_course_name.index("-") + 2
 
