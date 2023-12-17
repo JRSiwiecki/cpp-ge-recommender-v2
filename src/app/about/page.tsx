75e971b9816188e832e947f92a4c579b0cf1662d
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-16 23:01:04 -0800

    Style about page background and title

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <div>page</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-neutral-900 p-3">
+      <h1 className="text-center text-4xl text-white">About</h1>
+    </main>
+  );
 }
