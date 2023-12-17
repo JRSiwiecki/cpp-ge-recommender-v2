04e9ab955ed857aa17d9e1447ef522d637ab95e9
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-16 23:19:00 -0800

    Style sections a little more

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,71 +1,98 @@
+const linkStylingClasses = `text-center text-l 
+        text-yellow-400 flex items-center m-2 underline transition duration-300 hover:text-green-800`;
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-neutral-900 p-3">
       <h1 className="text-center text-4xl text-white">About</h1>
 
       <section>
-        <h1>Creator</h1>
-        <p>Made by Joseph Siwiecki</p>
+        <h2 className="m-1 text-center text-2xl text-white">Creator</h2>
+        <p className="m-1 text-center text-white">
+          Made by <strong>Joseph Siwiecki</strong>
+        </p>
 
-        <a
-          href="https://www.linkedin.com/in/josephsiwiecki/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
+        <div className="flex">
+          <a
+            className={linkStylingClasses}
+            href="https://www.linkedin.com/in/josephsiwiecki/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
 
-        <a
-          href="https://github.com/JRSiwiecki/cpp-ge-recommender-v2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github Repository
-        </a>
+          <a
+            className={linkStylingClasses}
+            href="https://github.com/JRSiwiecki/cpp-ge-recommender-v2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github Repository
+          </a>
+        </div>
       </section>
 
       <section>
-        <h2>Frameworks & Libraries</h2>
+        <h2 className="m-1 text-center text-2xl text-white">
+          Frameworks & Libraries
+        </h2>
 
-        <a
-          href="https://create.t3.gg/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          T3 Stack
-        </a>
+        <div className="flex">
+          <a
+            className={linkStylingClasses}
+            href="https://create.t3.gg/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            T3 Stack
+          </a>
 
-        <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">
-          Next.js
-        </a>
+          <a
+            className={linkStylingClasses}
+            href="https://nextjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Next.js
+          </a>
 
-        <a href="https://trpc.io/" target="_blank" rel="noopener noreferrer">
-          tRPC
-        </a>
+          <a
+            className={linkStylingClasses}
+            href="https://trpc.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            tRPC
+          </a>
 
-        <a
-          href="https://tailwindcss.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tailwind
-        </a>
+          <a
+            className={linkStylingClasses}
+            href="https://tailwindcss.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tailwind
+          </a>
 
-        <a
-          href="https://vercel.com/blog/what-is-vercel"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vercel
-        </a>
+          <a
+            className={linkStylingClasses}
+            href="https://vercel.com/blog/what-is-vercel"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vercel
+          </a>
 
-        <a
-          href="https://www.crummy.com/software/BeautifulSoup/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          BeautifulSoup (Scraper)
-        </a>
+          <a
+            className={linkStylingClasses}
+            href="https://www.crummy.com/software/BeautifulSoup/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            BeautifulSoup
+          </a>
+        </div>
       </section>
     </main>
   );
