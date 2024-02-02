890789c313e9c10fe80009008b9bacd55243c15d
JRSiwiecki <JRSiwiecki@gmail.com>
2024-02-02 14:05:44 -0800

    Call procedure from topCourses page

--- src/app/top-courses/page.tsx
+++ src/app/top-courses/page.tsx
@@ -1,7 +1,17 @@
+import { api } from "~/trpc/server";
+
 export default async function TopCourses() {
+  const topCourses = await api.topCourses.getTopCourses.query({
+    numberOfCourses: 5,
+  });
+
+  console.log(topCourses);
+
   return (
     <main className="flex min-h-screen flex-col items-center bg-neutral-900 p-3">
       <h1 className="text-center text-4xl text-white">Top Courses</h1>
+
+      <div className="grid grid-cols-3 gap-1"></div>
     </main>
   );
 }
