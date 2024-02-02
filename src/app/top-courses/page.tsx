55f081b440da7230687a5adb2e38836bc4317a25
JRSiwiecki <JRSiwiecki@gmail.com>
2024-02-01 20:14:11 -0800

    Add top courses route, update navbar

---
+++ src/app/top-courses/page.tsx
@@ -0,0 +1,7 @@
+export default async function TopCourses() {
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-neutral-900 p-3">
+      <h1 className="text-center text-4xl text-white">Top Courses</h1>
+    </main>
+  );
+}
