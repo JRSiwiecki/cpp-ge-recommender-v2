487aa06c4e537fedf2d71a94698c093ea9fdbf18
JRSiwiecki <JRSiwiecki@gmail.com>
2024-02-02 14:10:43 -0800

    Add metadata

--- src/app/top-courses/page.tsx
+++ src/app/top-courses/page.tsx
@@ -1,5 +1,12 @@
 import { api } from "~/trpc/server";
 
+export const metadata = {
+  title: "CPP GE Recommender",
+  description: `Top Courses page for CPP GE Recommender, 
+    containing the top GE courses by average GPA for each Area + Section at CPP.`,
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
 export default async function TopCourses() {
   const topCourses = await api.topCourses.getTopCourses.query({
     numberOfCourses: 5,
