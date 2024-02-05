94b7fc7d46898cc3c4db0cc54ab7f5f4a210feee
JRSiwiecki <JRSiwiecki@gmail.com>
2024-02-04 16:41:52 -0800

    Add very very crude top courses page

--- src/app/top-courses/page.tsx
+++ src/app/top-courses/page.tsx
@@ -1,5 +1,7 @@
 import { api } from "~/trpc/server";
 
+import type { TopCourses } from "~/types/courseDataTypes";
+
 export const metadata = {
   title: "CPP GE Recommender Top Courses",
   description: `Top Courses page for CPP GE Recommender, 
@@ -7,17 +9,38 @@ export const metadata = {
 };
 
 export default async function TopCourses() {
-  const topCourses = await api.topCourses.getTopCourses.query({
-    numberOfCourses: 5,
-  });
+  const queryResult = await api.topCourses.getTopCourses.query();
+
+  const topCourses: TopCourses = queryResult.topCourses;
 
   console.log(topCourses);
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-neutral-900 p-3">
       <h1 className="text-center text-4xl text-white">Top Courses</h1>
-
-      <div className="grid grid-cols-3 gap-1"></div>
+      <div className="grid grid-cols-3 gap-1">
+        {topCourses.areas.map((area) => (
+          <div key={area.area} className="mb-4 rounded-md border">
+            <h2 className="text-xl font-semibold text-white">{area.area}</h2>
+            <div className="grid grid-cols-2 gap-1">
+              {area.sections.map((section) => (
+                <div key={section.section} className="mb-2 rounded-md border">
+                  <h3 className="text-lg font-semibold text-white">
+                    {section.section}
+                  </h3>
+                  <ul>
+                    {section.courses.map((course) => (
+                      <li key={course.courseCode} className="text-white">
+                        {course.courseCode} - {course.averageGPA}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
