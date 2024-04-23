02e5785e76b2a594193810d7282907ad7c7692eb
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-22 23:38:46 -0700

    Add metadata for top courses page

---
+++ src/app/top-courses/layout.tsx
@@ -0,0 +1,5 @@
+export const metadata = {
+  title: "CPP GE Recommender Top Courses",
+  description: `Top Courses page for CPP GE Recommender, 
+    containing the top GE courses by average GPA for each Area + Section at CPP.`,
+};
